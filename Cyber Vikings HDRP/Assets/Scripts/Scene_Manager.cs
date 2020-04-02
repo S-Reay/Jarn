eb593f041a6180bc022bc167e09253201ea536b7
S-Reay <1017464@student.sae.edu.au>
2020-04-02 15:23:03 +1100

    Hard Reset Local Commits, Fixed gitIgnore
    Used GitBash to hard reset my local, in order to remove the commits containing the problematic large file. Fixed a type in gitIgnore.

---
+++ Cyber Vikings HDRP/Assets/Scripts/Scene_Manager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Manager : MonoBehaviour
+{
+    public void LoadScene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }   
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}

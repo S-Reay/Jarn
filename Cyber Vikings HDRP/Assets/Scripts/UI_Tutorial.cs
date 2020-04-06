268baa4c591e45fda8d62e46248ea2a0f903a8bf
Anne CAves <annecaves90@gmail.com>
2020-04-06 14:12:44 +1000

    AC_BeachEdit
    tutorial checkpoints added textures added terrain edited

--- Cyber Vikings HDRP/Assets/Scripts/UI_Tutorial.cs
+++ Cyber Vikings HDRP/Assets/Scripts/UI_Tutorial.cs
@@ -7,34 +7,24 @@ public class UI_Tutorial : MonoBehaviour
 {
     public GameObject tutorialPanel;
     public GameObject text;
-    public bool isFinal = false;
-    float endTimer = 5f;
+    public GameObject meleeObject;
 
-    public void Start()
+    private void start()
     {
-        tutorialPanel = GameObject.FindGameObjectWithTag ("Tutorial");
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            text.SetActive(true);
-            foreach (GameObject text in GameObject.FindGameObjectsWithTag("TutorialText"))
-            {
-                text.SetActive(false);
-            }
-            text.SetActive(true);
-            if (isFinal)
-            {
-                StartCoroutine(Countdown());
-            }
-        }
-
-    }
-    private IEnumerator Countdown()
-    {
-        yield return new WaitForSeconds(5);
         tutorialPanel.SetActive(false);
+        text.SetActive(false);
+    }
+    public void OnTriggerEnter()
+    {
+        tutorialPanel.SetActive(true);
+        text.SetActive(true);
+        meleeObject.SetActive(true);
+    }
+
+    public void OnTriggerExit()
+    {
+        tutorialPanel.SetActive(false);
+        text.SetActive(false);
+        meleeObject.SetActive(false);
     }
 }

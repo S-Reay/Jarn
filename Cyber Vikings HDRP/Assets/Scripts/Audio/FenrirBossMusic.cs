edafb7020a1f06a05f366fc8a2aec6b59efeef4e
Harlem Egen <1017058@student.sae.edu.au>
2020-04-06 11:11:41 +1000

    Added audio files, Boss fight music added

---
+++ Cyber Vikings HDRP/Assets/Scripts/Audio/FenrirBossMusic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FenrirBossMusic : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Player Has Entered Trigger");
+            GetComponent<AudioSource>().Play();
+        }
+    }
+    //void OnTriggerExit(Collider other)
+    //{
+        //if (other.tag == "Player")
+        //{
+            //GetComponent<AudioSource>().Stop();
+        //}
+    //}
+}

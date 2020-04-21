686e49ed2c74e63e5a4a4a6e8a9c2c7a5c0c24e1
Harlem E <1017058@student.sae.edu.au>
2020-04-21 11:00:49 +1000

    Cutscene Prepared (not completed)

---
+++ Cyber Vikings HDRP/Assets/Cutscene/CutsceneTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CutsceneTrigger : MonoBehaviour
+{
+    public PlayableDirector cutscene;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        cutscene.Play();
+    }
+}

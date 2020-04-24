4d6f05b10049c38aabfd7e7511d1174ad4f3975c
Anne CAves <annecaves90@gmail.com>
2020-04-25 01:43:29 +1000

    Credit Screen. Enemies Relocated Weapon Drop script finalised

--- Cyber Vikings HDRP/Assets/Scripts/EnemyAI/AC_EnemyWeaponDrop.cs
+++ Cyber Vikings HDRP/Assets/Scripts/EnemyAI/AC_EnemyWeaponDrop.cs
@@ -6,25 +6,23 @@ public class AC_EnemyWeaponDrop : MonoBehaviour
 {
     public GameObject weapon;
     EnemyHealth enemyHealthScript;
+    public Transform weaponPosition;
+    public bool dead = false;
 
     // Start is called before the first frame update
     void Start()
     {
         enemyHealthScript = GameObject.FindGameObjectWithTag("Enemy").GetComponent<EnemyHealth>();
     }
-    private void Awake()
-    {
-        weapon.SetActive(false);
-    }
-
-
+     
     
     // Update is called once per frame
     void Update()
     {
         if (enemyHealthScript.dead)
         {
-            weapon.SetActive(true);
+            dead = true; 
+            Instantiate(weapon, weaponPosition.position, weaponPosition.rotation);
         }
     }
 }

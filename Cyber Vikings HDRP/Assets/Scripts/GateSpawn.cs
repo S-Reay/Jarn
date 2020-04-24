af3dd86996103c9e929d62a9fd9be651941e13da
Harlem E <1017058@student.sae.edu.au>
2020-04-24 22:17:37 +1000

    Added Gate, Open/Close Chest audio (not on chest)

---
+++ Cyber Vikings HDRP/Assets/Scripts/GateSpawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateSpawn : MonoBehaviour
+{
+    public MeshRenderer mesh;
+    public BoxCollider trigger;
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            mesh = gameObject.GetComponent<MeshRenderer>();
+            mesh.enabled = true;
+            trigger = gameObject.GetComponent<BoxCollider>();
+            trigger.isTrigger = false;
+        }
+    }
+}

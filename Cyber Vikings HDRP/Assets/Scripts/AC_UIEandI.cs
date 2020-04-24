7f8b5c25dc8bad8963032c00c6a10f5c0594e5e7
Anne CAves <annecaves90@gmail.com>
2020-04-24 16:13:40 +1000

    Environmental Assets Moved Relic Added Rune Flames Edited UI Assets for Runes Added

---
+++ Cyber Vikings HDRP/Assets/Scripts/AC_UIEandI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class AC_UIEandI : MonoBehaviour
+{
+    [SerializeField] private Image EButtonImage;
+    [SerializeField] private Image IButtonImage;
+
+    public void Awake()
+    {
+        EButtonImage.enabled = false;
+        IButtonImage.enabled = false;
+
+    }
+
+    public void OnTriggerEnter(Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            EButtonImage.enabled = true;
+            IButtonImage.enabled = false;
+
+            Debug.Log("player entered trigger");
+        }
+
+
+    }
+
+    public void OnTriggerExit(Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            EButtonImage.enabled = false;
+            IButtonImage.enabled = false;
+            
+            Debug.Log("player exited trigger");
+
+        }
+
+    }
+}

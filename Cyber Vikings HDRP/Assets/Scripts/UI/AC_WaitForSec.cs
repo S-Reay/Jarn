21ed5ca2266117d04699bfc218f4c43c187afeb6
Anne CAves <annecaves90@gmail.com>
2020-04-30 18:18:31 +1000

    UI Turotial Start Delay For Cut Scene

---
+++ Cyber Vikings HDRP/Assets/Scripts/UI/AC_WaitForSec.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AC_WaitForSec : MonoBehaviour
+{
+    [SerializeField] private Image tutorialPanel;
+    [SerializeField] private Text text;
+
+    public void Awake()
+    {
+        tutorialPanel.enabled = false;
+        text.enabled = false;
+        StartCoroutine("WaitForSec");
+    }
+
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(10);
+        tutorialPanel.enabled = true;
+        text.enabled = true;
+
+        Debug.Log("coroutine started");
+    }
+
+
+      
+
+    public void OnTriggerExit(Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            tutorialPanel.enabled = false;
+            text.enabled = false;
+            Debug.Log("player entered trigger");
+
+        }
+
+    }
+
+}
